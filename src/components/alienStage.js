e1601a6504a2f7af6bdeec5d4cb4179f8945ee88
Alex Nudelman <nudelx@gmail.com>
2018-03-29 23:19:07 +0300

    before move all management to the game box

--- src/components/alienStage.js
+++ src/components/alienStage.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import Alien from './alien'
-import { ALIENS } from '../constants/aliensConst'
+
 
 class AlienStage extends Component {
-
+  state = { aliens: ALIENS }
   componentDidMount () {
       console.log('AlienStage => done on mount ', this.refs.alienStage)
       const { offsetTop, offsetHeight} = this.refs.alienStage
@@ -14,13 +14,16 @@ class AlienStage extends Component {
       this.props.setAppState(position)
   }
 
+  checkHit () {
+
+  }
 
   render() {
     console.log('AlienStage')
     return (
       <div ref='alienStage' className="alien-stage">
         <div className="alien-box">
-            { ALIENS.map( (a, i) => <Alien key={i}/>)}
+            { this.state.aliens.map( a => <Alien ref={a.alienId} key={a.alienId}/>)}
         </div>
 
       </div>
