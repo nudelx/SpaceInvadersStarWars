635dd4163c4b9bbc74e800a644c7f5fbf2ffcf0f
nudelx <alex.nudelman@samanage.com>
2018-05-08 13:23:08 +0300

    in

--- src/components/alienStage.js
+++ src/components/alienStage.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react"
-import Alien from "./alien"
-import { ALIENS } from "../constants/aliensConst"
+import React, { Component } from 'react'
+import Alien from './alien'
+import { ALIENS } from '../constants/aliensConst'
 
 class AlienStage extends Component {
   state = { aliens: ALIENS }
   componentDidMount() {
-    console.log("AlienStage => done on mount ", this.refs.alienStage)
+    console.log('AlienStage => done on mount ', this.refs.alienStage)
     const { offsetTop, offsetHeight } = this.refs.alienStage
     const position = {
       alienStageBottom: parseInt(offsetTop, 10) + parseInt(offsetHeight, 10),
@@ -13,7 +13,17 @@ class AlienStage extends Component {
       alienHitCheck: this.alienHitCheck
     }
     this.props.setBoxState(position)
+  }
 
+  checkByX(alienRef, x) {
+    return (
+      alienRef &&
+      alienRef.alien.offsetLeft + alienRef.alien.offsetParent.offsetLeft < x &&
+      x <
+        alienRef.alien.offsetLeft +
+          alienRef.alien.offsetParent.offsetLeft +
+          alienRef.alien.offsetWidth
+    )
   }
 
   alienHitCheck = props => {
@@ -25,14 +35,17 @@ class AlienStage extends Component {
       // console.log(alienRef)
       // console.log("x", x)
       // console.log("y", y)
-      if ( alienRef && alienRef.alien.offsetLeft + alienRef.alien.offsetParent.offsetLeft < x  && x < alienRef.alien.offsetLeft + alienRef.alien.offsetParent.offsetLeft + alienRef.alien.offsetWidth) {
-        // console.log('da', alienRef)
+      if (this.checkByX(alienRef, x)) {
+        console.log('da X HIT !!!!', alienRef)
+        if (this.checkByY(alienRef, y)) {
+          console.log('KILLL')
+        }
       }
     })
   }
 
   render() {
-    console.log("AlienStage")
+    console.log('AlienStage')
     return (
       <div ref="alienStage" className="alien-stage">
         <div className="alien-box">
